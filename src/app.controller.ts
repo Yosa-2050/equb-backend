dd346b775722fb0e9d61b60a86b9f9990ff8c134
yoseph <yoseph7091@gmail.com>
2026-08-17 03:07:32 -0100

    feat:cron ping sleep

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from './auth/current-user.decorator';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // Public so uptime pingers (keeping the free-tier host from sleeping) get
+  // a clean 200 instead of a 401.
+  @Public()
   @Get()
   getHello(): string {
     return this.appService.getHello();
